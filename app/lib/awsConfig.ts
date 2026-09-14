01085174851dabc8b56a142e8cbb34d6aed344c4
Kaushik <koushik@harmonyflow.ai>
2026-09-14 15:13:35 -0400

    sigin

--- app/lib/awsConfig.ts
+++ app/lib/awsConfig.ts
@@ -2,7 +2,8 @@ export const API_BASE_URL =
   "https://8scq4w84j2.execute-api.us-east-1.amazonaws.com/prod";
 
 export const COGNITO_REGION = "us-east-1";
-export const COGNITO_CLIENT_ID = "5gro2t44jv4sdiifls6kgq96q6";
+export const COGNITO_CLIENT_ID: string =
+  process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID || "5gro2t44jv4sdiifls6kgq96q6";
 
 export const DASHBOARD_DATA_URL =
   process.env.NEXT_PUBLIC_DASHBOARD_DATA_URL || `${API_BASE_URL}/dashboard`;
